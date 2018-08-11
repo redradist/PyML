99736c006589b52f31c9227868ffb22e52ee786b
redradist <redradist@gmail.com>
2018-08-11 21:55:20 +0300

    Added first attempt to implement automatic convergence to optimal value

--- pyml/regression/models/linear.py
+++ pyml/regression/models/linear.py
@@ -10,7 +10,7 @@ class Linear:
     params_index = 0
     result_index = 1
 
-    def __init__(self, *training_examples):
+    def __init__(self, *training_examples, alpha=None, max_num_of_attempts=10000):
         """
         Method for initializing the object LinearRegression algorithm for the following model
         θ0*x0 + θ1*x1 + ... + θn*xn = y
@@ -21,7 +21,8 @@ class Linear:
         self._number_of_params = Linear.valid_training_examples(*training_examples) + 1
         self._training_examples = tuple(((1, *params), results) for params, results in training_examples)
         self._thetas = [1 for x in range(self._number_of_params)]
-        self._alpha = None
+        self._alpha = alpha
+        self._max_num_of_attempts = max_num_of_attempts
 
     @staticmethod
     def valid_training_examples(*training_examples):
@@ -53,20 +54,48 @@ class Linear:
         self._alpha = alpha
 
     @property
+    def max_num_of_attempts(self):
+        return self._max_num_of_attempts
+
+    @max_num_of_attempts.setter
+    def max_num_of_attempts(self, num_of_attempts):
+        self._max_num_of_attempts = num_of_attempts
+
+    @property
     def thetas(self):
         return list(self._thetas)
 
-    def calculate(self):
+    def perform(self):
         origin_thetas = list(self._thetas)
-        continiou = True
-        while continiou:
+        if self._alpha is None:
+            self._alpha = 0.1
+        number_of_attempts = 0
+        proceed = True
+        prev_step = None
+        prev_relative_changes = None
+        while proceed:
             try:
-                self._alpha = 0.1
+                prev_step = list(self._thetas)
                 self.perform_one_step()
+                relative_changes = sum(abs(prev_theta - cur_theta) for prev_theta, cur_theta in zip(prev_step, self._thetas)) / \
+                                   sum(abs(cur_theta) for cur_theta in self._thetas)
+
+                if prev_relative_changes is not None and relative_changes > prev_relative_changes:
+                    if number_of_attempts < self._max_num_of_attempts:
+                        self._alpha /= 3
+                    else:
+                        raise ValueError('Exceeded number of attempts to find the optimal parameters !!')
+                elif relative_changes < 0.000000000001:
+                    proceed = False
+                prev_relative_changes = relative_changes
             except ValueError as ex:
-                pass
-        else:
-            self._thetas = origin_thetas
+                if 'NaN value !!' in ex.args[0]:
+                    if number_of_attempts < self._max_num_of_attempts:
+                        self._alpha /= 3
+                        self._thetas = list(origin_thetas)
+                        number_of_attempts += 1
+                    else:
+                        raise ValueError('Exceeded number of attempts to find the optimal parameters !!')
 
     def perform_one_step(self):
         temp_thetas = list(range(self._number_of_params))
@@ -82,6 +111,6 @@ class Linear:
                 sum_of_examples += (hyp_res - res) * x_index_theta
             temp_thetas[index_theta] = self._thetas[index_theta] - self._alpha * sum_of_examples / num_examples
             if math.isnan(temp_thetas[index_theta]):
-                raise ValueError(f'Value of theta[index_theta={index_theta}] is exceeded the limit !!')
+                raise ValueError(f'NaN value !! Value of theta[index_theta={index_theta}] is exceeded the limit !!')
             index_theta += 1
         self._thetas = temp_thetas
--- tests/regression/models/linear_regression_tests.py
+++ tests/regression/models/linear_regression_tests.py
@@ -49,3 +49,22 @@ class Testing_LinearRegression(unittest.TestCase):
                                places=5,
                                msg='Values are not equals !! '
                                    'Linear regression has not found proper prediction for test data !!')
+
+    def test__LinearRegressionAutomatic__Valid(self):
+        training_examples = [
+            ((1, 2), 0.8),
+            ((4, 2), 1.4),
+            ((4, 9), 3.5)
+        ]
+        alg = Linear(alpha=0.01,
+                     max_num_of_attempts = 100000,
+                     *training_examples)
+        alg.perform()
+        theta0 = alg.thetas[0]
+        theta1 = alg.thetas[1]
+        theta2 = alg.thetas[2]
+        self.assertAlmostEqual(theta0 + theta1 * 16 + theta2 * 8,
+                               5.6,
+                               places=5,
+                               msg='Values are not equals !! '
+                                   'Linear regression has not found proper prediction for test data !!')
